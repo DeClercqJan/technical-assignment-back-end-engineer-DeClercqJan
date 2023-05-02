0ee449101e94f3d893b497b57e958ca9986493b2
declercqjan <declercqjan@gmail.com>
2023-05-02 15:28:32 +0200

    create entity + migration using attributes

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -1,8 +1,47 @@
 <?php
 
-namespace App\Model;
+namespace App\Entity;
 
+use App\Repository\ProductRepository;
+use Doctrine\ORM;
+use Ramsey\Uuid\UuidInterface;
+
+#[ORM\Mapping\Entity(repositoryClass: ProductRepository::class)]
+#[ORM\Mapping\Table(name: "products")]
 class Product
 {
+    #[ORM\Mapping\Id]
+    #[ORM\Mapping\Column(type: "string", unique: true, nullable: false)]
+    private UuidInterface $uuid;
 
+    #[ORM\Mapping\Column(type: "string", unique: true, nullable: false)]
+    private string $name;
+
+    #[ORM\Mapping\Column(type: "integer", nullable: false)]
+    private int $price;
+
+
+    public function __construct(UuidInterface $uuid, string $name, int $price)
+    {
+        $this->uuid = $uuid;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getUuid(): UuidInterface
+    {
+        return $this->uuid;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
 }
+
+
